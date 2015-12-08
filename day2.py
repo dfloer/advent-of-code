deeb5c63aa7bae9d5c8a7a5b1ba4c592694a689d
Dale Floer <dalefloer@gmail.com>
2015-12-07 18:44:34 -0800

    Solution to day2 pt1.

---
+++ day2.py
@@ -0,0 +1,12 @@
+def day2_split():
+    with open('day2.txt', 'r') as f:
+        lines = f.readlines()
+    return [[int(y) for y in x.split('x')] for x in lines]
+
+
+def day2():
+    boxes = day2_split()
+    for x in boxes:
+        x.sort()
+    total = sum([(x[0] * x[1]) * 3 + (x[1] * x[2]) * 2 + (x[2] * x[0]) * 2 for x in boxes])
+    return total
