820986408d13639cfb41e0ae3a5ccaf6282f4b7d
Dale Floer <dalefloer@gmail.com>
2015-12-08 12:06:40 -0800

    Solution for day5 pt2.

--- day5.py
+++ day5.py
@@ -8,7 +8,7 @@ def day5_split():
     return lines
 
 
-def day5():
+def day5_pt1():
     words = day5_split()
     nice = [is_nice(x) for x in words]
     return nice.count(True)
@@ -35,3 +35,28 @@ def is_nice(word):
         return True
     else:
         return False
+
+
+def day5_pt2():
+    words = day5_split()
+    nice = [is_nice2(x) for x in words]
+    return nice.count(True)
+
+
+def is_nice2(word):
+    pairs = False
+    repeated = False
+
+    for idx in range(len(word) - 1):
+        word2 = word[0 : idx] + '00' + word[idx + 2 :]
+        letters = word[idx : idx + 2]
+        if word2.find(letters) != -1:
+            pairs = True
+
+    for idx in range(len(word) - 2):
+        if word[idx] == word[idx + 2]:
+            repeated = True
+
+    if pairs and repeated:
+        return True
+    return False
