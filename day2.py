28dd462e11a55cebbdf10cee61e216e0f1ae09f8
Dale Floer <dalefloer@gmail.com>
2015-12-07 18:50:01 -0800

    Added solution for day2 pt2.

--- day2.py
+++ day2.py
@@ -8,5 +8,6 @@ def day2():
     boxes = day2_split()
     for x in boxes:
         x.sort()
-    total = sum([(x[0] * x[1]) * 3 + (x[1] * x[2]) * 2 + (x[2] * x[0]) * 2 for x in boxes])
-    return total
+    total_paper = sum([(x[0] * x[1]) * 3 + (x[1] * x[2]) * 2 + (x[2] * x[0]) * 2 for x in boxes])
+    total_ribbon = sum([(x[0] * x[1] * x[2]) + 2 * (x[0] + x[1]) for x in boxes])
+    return total_paper, total_ribbon
