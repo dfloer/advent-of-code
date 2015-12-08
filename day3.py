36532a53852a74bde0d7d3ee990099b689a854fc
Dale Floer <dalefloer@gmail.com>
2015-12-07 20:18:00 -0800

    Solution to day3 part2.

--- day3.py
+++ day3.py
@@ -4,15 +4,28 @@ def day3_split():
     return list(lines)
 
 
-def day3():
+def day3_pt1():
     actions = day3_split()
+    return len(day3_common(actions))
+
+
+def day3_common(actions):
     start = (0, 0)
     house = set()
     for a in actions:
         end = tuple([sum(x) for x in zip(start, action_to_xy(a))])
         house.add(end)
         start = end
-    return len(house)
+    return house
+
+
+def day3_pt2():
+    actions = day3_split()
+    santa = actions[0:][::2]
+    robo_santa = actions[1:][::2]
+    santa_count = day3_common(santa)
+    robo_santa_count = day3_common(robo_santa)
+    return len(santa_count.union(robo_santa_count))
 
 
 def action_to_xy(action):
