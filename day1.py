4d7f2c911a333046f25616d246a00a9c0994e742
Dale Floer <dalefloer@gmail.com>
2015-12-07 17:44:03 -0800

    Added solution for day1, part2.

--- day1.py
+++ day1.py
@@ -2,3 +2,13 @@ def day_1_pt1(directions):
     num_up = directions.count('(')
     num_down = directions.count(')')
     return num_up - num_down
+
+def day1_pt2(directions):
+    numbers = [1 if x == '(' else -1 for x in directions]
+    idx = 0
+    total = 0
+    while(True):
+        total += numbers[idx]
+        if total == -1:
+            return idx + 1  # +1 needed because using 1 based index in problem.
+        idx += 1
