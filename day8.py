2b422964b067e02ebe1f11b11fb96a4e53b06f96
Dale Floer <dalefloer@gmail.com>
2015-12-07 23:12:28 -0800

    Solution day8 pt2.

--- day8.py
+++ day8.py
@@ -1,5 +1,5 @@
 import ast
-
+import json
 
 def day8_split():
     with open('day8.txt', 'r') as f:
@@ -7,10 +7,19 @@ def day8_split():
     return lines
 
 
-def day8():
+def day8_pt1():
     lines = day8_split()
     lines = [x.replace('\n', '') for x in lines]
     total_chars = sum(len(x) for x in lines)
     strings = [ast.literal_eval(x) for x in lines]
     string_chars = sum([len(x) for x in strings])
     return total_chars - string_chars
+
+
+def day8_pt2():
+    lines = day8_split()
+    lines = [x.replace('\n', '') for x in lines]
+    total_chars = sum(len(x) for x in lines)
+    strings = [json.dumps(x) for x in lines]
+    string_chars = sum([len(x) for x in strings])
+    return string_chars - total_chars
