3df1b907f083d73c4c74907e59f2ce693d9d35e2
Dale Floer <dalefloer@gmail.com>
2015-12-11 23:56:49 -0800

    Solution for day12 part2.

--- day12.py
+++ day12.py
@@ -1,11 +1,18 @@
+import json
+
+
 def day12_split():
     with open('day12.txt', 'r') as f:
         lines = f.read()
     return lines
 
-def day12():
-    a = day12_split()
 
+def day12_pt1():
+    a = day12_split()
+    return day12_count(a)
+
+
+def day12_count(a):
     l = ''
     for idx in range(len(a)):
         x = a[idx]
@@ -17,3 +24,23 @@ def day12():
             l += ' '
     ln = l.split()
     return sum([int(x)for x in ln])
+
+
+def day12_pt2():
+    a = day12_split()
+    j = json.loads(a)
+    x = day12_pt2_parse(j)
+    js = json.dumps(x)
+    return day12_count(js)
+
+
+def day12_pt2_parse(obj):
+    if isinstance(obj, list):
+        return [day12_pt2_parse(x) for x in obj]
+    elif isinstance(obj, dict):
+        if 'red' in obj.values():
+            return dict()
+        else:
+            return {k: day12_pt2_parse(v) for k, v in obj.items()}
+    else:
+        return obj
