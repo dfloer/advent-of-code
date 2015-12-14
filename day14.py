e060d2d44628cfdbdc7c0f272f7bf729b55e3f28
Dale Floer <dalefloer@gmail.com>
2015-12-13 23:16:14 -0800

    Solution for day14 pt2.

--- day14.py
+++ day14.py
@@ -15,7 +15,7 @@ def day14_parse():
     return d
 
 
-def day14(t):
+def day14_pt1(t):
     d = day14_parse()
     # d = {'Comet': [14, 10, 127], 'Dancer': [16, 11, 162]}
     dist = {k: v + v for k, v in d.items()}
@@ -33,4 +33,33 @@ def day14(t):
     print(sorted(res.items(), key=operator.itemgetter(1)))
 
 
+def day14_pt2(t):
+    d = day14_parse()
+    # d = {'Comet': [14, 10, 127], 'Dancer': [16, 11, 162]}
+    dist = {k: v + v for k, v in d.items()}
+    res = {k: [0] for k in d.keys()}
+    points = {k: 0 for k in d.keys()}
+    for x in range(t):
+        for k, v in dist.items():
+            if v[4] == 0 and v[5] == 0:  # Reindeer is done going and resting, go back to both.
+                v[4] = v[1]
+                v[5] = v[2]
+            if v[4] == 0:  # Reindeer is resting, countdown their resting clock
+                v[5] -= 1
+                res[k] += [res[k][-1]]
+            else:  # Reindeer is moving and not resting.
+                v[4] -= 1
+                res[k] += [res[k][-1] + v[0]]
+
+        distances = [(a, b[-1]) for a, b in res.items()]
+        m = max(distances, key=lambda y: y[1])[1]
+
+        i = []
+        for a, b in distances:
+            if m == b:
+                i.append(a)
+        for deer in i:
+            points[deer] += 1
+
+    return max(points.items(), key=lambda p: p[1])
 
