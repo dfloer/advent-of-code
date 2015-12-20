4db7d6bb3a52b29078c5291a3c28b2f118208fd8
Dale Floer <dalefloer@gmail.com>
2015-12-19 22:53:16 -0800

    Solution to day20 pt2.

--- day20.py
+++ day20.py
@@ -1,7 +1,8 @@
 from itertools import chain
 from math import sqrt
 
-def day20(target):
+
+def day20_pt1(target):
     idx = 0
     while True:
         factors = find_factors(idx)
@@ -10,9 +11,19 @@ def day20(target):
         idx += 1
 
 
+def day20_pt2(target):
+    idx = 0
+    while True:
+        factors = find_factors(idx)
+        presents = [x for x in factors if idx / x <= 50]
+        if sum(presents) * 11 >= target:
+            return idx
+        idx += 1
+
+
 def find_factors(n):
     """
-    Returns the facrtors of n.
+    Returns the factors of n.
     """
     factors = set(chain.from_iterable((i, n // i) for i in range(1, int(sqrt(n)) + 1) if n % i == 0))
     return factors
