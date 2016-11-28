48068d98164686b286764cf91bf4db8821659ff0
Dale Floer <dalefloer@gmail.com>
2016-11-28 00:12:55 -0800

    Better solution to day25 part1.

--- 2015/day25.py
+++ 2015/day25.py
@@ -1,10 +1,5 @@
 def day25(row, column, initial_value, mult, div):
     total_numbers = int(((row + column - 2) * (row + column - 1)) / 2 + column - 1)
-    print(row, column, total_numbers, mult, div)
-    value = initial_value
-    # I feel like there's a simple mathematical answer to this.
-    for x in range(total_numbers):
-        value = (value * mult) % div
-    print(value)
-
-
+    # We need to use modular exponentiation for this: https://en.wikipedia.org/wiki/Modular_exponentiation
+    value = initial_value * pow(mult, total_numbers, div) % div
+    return value
