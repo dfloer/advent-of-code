9a5e737cc8271e31148099606654eefa0b4cc8e3
Dale Floer <dalefloer@gmail.com>
2016-11-30 22:10:52 -0800

    Solution to day1 part2.

--- 2016/day1.py
+++ 2016/day1.py
@@ -1,13 +1,16 @@
+from collections import OrderedDict
+
 def day1_split():
     with open('day1.txt', 'r') as f:
         lines = f.read().split(', ')
     return lines
 
 
-def day1():
+def day1(part1=True):
     lines = day1_split()
     loc = [0, 0]
     direction = 0
+    visited = OrderedDict()
     for i in lines:
         turn = i[0 : 1]
         amount = int(i[1 :])
@@ -17,15 +20,21 @@ def day1():
         else:
             direction = (direction - 1) % 4
 
+        for _ in range(amount):
+            if direction == 0:
+                loc[0] += 1
+            elif direction == 2:
+                loc[0] -= 1
+            elif direction == 1:
+                loc[1] += 1
+            else:
+                loc[1] -= 1
 
-        if direction == 0:
-            loc[0] += amount
-        elif direction == 2:
-            loc[0] -= amount
-        elif direction == 1:
-            loc[1] += amount
-        else:
-            loc[1] -= amount
+            if not part1:
+                key = tuple(loc)
+                if key in visited.keys():
+                    return abs(loc[0]) + abs(loc[1])
+                else:
+                    visited[key] = 1
 
-    print(abs(loc[0]) + abs(loc[1]))
-
+    return abs(loc[0]) + abs(loc[1])
