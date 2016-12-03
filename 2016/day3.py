fcd319472adae4938f50993a8d3e8e2add8a487b
Dale Floer <dalefloer@gmail.com>
2016-12-02 22:24:02 -0800

    Cleanup.

--- 2016/day3.py
+++ 2016/day3.py
@@ -4,7 +4,7 @@ def day3_split():
     return lines
 
 
-def day3():
+def day3_part1():
     triangles = [x.split() for x in day3_split()]
     count = 0
     total = len(triangles)
@@ -18,11 +18,14 @@ def day3():
 def day3_part2():
     triangles = [x.split() for x in day3_split()]
     count = 0
+    # Basically creates rows from the columns, though they turn out backwards.
     rotated = list(zip(*triangles[::-1]))
     big_list = []
+    # Sticks the three different rows into one big row, because they're all the same size.
     for x in rotated:
         big_list += x
     total = len(big_list) // 3
+    # Iterate over the list 3 values at a time and check the inequality as above.
     for idx in range(0, len(big_list), 3):
         triangle = big_list[idx : idx + 3]
         sides = sorted([int(y) for y in triangle])
