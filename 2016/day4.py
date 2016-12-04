77a1ff281e2fc49c4f3977571e1ecc3a0c90133e
Dale Floer <dalefloer@gmail.com>
2016-12-03 23:04:19 -0800

    Added solution to day4 part2.

--- 2016/day4.py
+++ 2016/day4.py
@@ -6,7 +6,7 @@ def day4_split():
 
     return lines
 
-def day4():
+def day4_part1():
     stuff = day4_split()
     ids = []
     for x in stuff:
@@ -21,3 +21,20 @@ def day4():
         if calc_sum == checksum:
             ids += [room_id]
     return sum(ids)
+
+
+def day4_part2():
+    stuff = day4_split()
+    for x in stuff:
+        line = x.split('[')
+        room = ''.join(line[0][:-4].split('-'))
+        room_id = int(line[0][-3:])
+        room_text = caesar_cipher(room, room_id % 26)
+        if room_text.startswith("northpole"):
+            return room, room_text, room_id
+
+
+def caesar_cipher(cipher_text, shift):
+    # 97 is the index in ASCII for 'a'.
+    decrypted = [chr(((ord(c) - 97 + shift) % 26) + 97) for c in cipher_text]
+    return ''.join(decrypted)
