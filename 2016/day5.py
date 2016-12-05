1ad95f28cffa3f54d7f5684de9cd619eb000e999
Dale Floer <dalefloer@gmail.com>
2016-12-04 22:26:10 -0800

    Added solution to day5 part2.

--- 2016/day5.py
+++ 2016/day5.py
@@ -5,10 +5,10 @@ def day5_part1(input_text):
     password = []
     idx = 0
     while True:
-        hash = md5()
+        h = md5()
         to_hash = input_text + str(idx)
-        hash.update(to_hash.encode('utf-8'))
-        candidate = hash.hexdigest()
+        h.update(to_hash.encode('utf-8'))
+        candidate = h.hexdigest()
         if candidate[:5] == '00000':
             password += [candidate[5]]
         idx += 1
@@ -16,3 +16,29 @@ def day5_part1(input_text):
             break
 
     return''.join(password)
+
+
+def day5_part2(input_text):
+    password = [''] * 8
+    idx = 0
+    while True:
+        h = md5()
+        to_hash = input_text + str(idx)
+        h.update(to_hash.encode('utf-8'))
+        candidate = h.hexdigest()
+        if candidate[:5] == '00000':
+            try:
+                posn = int(candidate[5])
+                if password[posn] == '':
+                    password[posn] = candidate[6]
+            except IndexError:
+                # Handles the case when we already have an index that's too high.
+                pass
+            except ValueError:
+                # Handles indexes that aren't possible.
+                pass
+        idx += 1
+        if password.count('') == 0:
+            break
+
+    return ''.join(password)
