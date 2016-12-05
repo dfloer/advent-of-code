272330bc03fd30786b75163566b035a3f8e7eab8
Dale Floer <dalefloer@gmail.com>
2016-12-04 16:45:20 -0800

    Code cleanup.

--- 2016/day4.py
+++ 2016/day4.py
@@ -1,19 +1,18 @@
 from collections import Counter
 
+
 def day4_split():
     with open('day4.txt', 'r') as f:
         lines = f.read().splitlines()
 
     return lines
 
+
 def day4_part1():
     stuff = day4_split()
     ids = []
     for x in stuff:
-        line = x.split('[')
-        checksum = line[1][:-1]
-        room = ''.join(line[0][:-4].split('-'))
-        room_id = int(line[0][-3:])
+        room, room_id, checksum = parse_line(x)
         counts = Counter(room)
         # Sort the dictionary according to the key and then value. Puts the largest counts first, sorted alphabetically.
         sorted_counts = [v[0] for v in sorted(counts.items(), key=lambda kv: (-kv[1], kv[0]))][:5]
@@ -26,9 +25,7 @@ def day4_part1():
 def day4_part2():
     stuff = day4_split()
     for x in stuff:
-        line = x.split('[')
-        room = ''.join(line[0][:-4].split('-'))
-        room_id = int(line[0][-3:])
+        room, room_id, _ = parse_line(x)
         room_text = caesar_cipher(room, room_id % 26)
         if room_text.startswith("northpole"):
             return room, room_text, room_id
@@ -38,3 +35,15 @@ def caesar_cipher(cipher_text, shift):
     # 97 is the index in ASCII for 'a'.
     decrypted = [chr(((ord(c) - 97 + shift) % 26) + 97) for c in cipher_text]
     return ''.join(decrypted)
+
+
+def parse_line(line):
+    # Split into the room+id and checksum.
+    room_data, checksum = line.split('[')
+    # Remove the '-' from the input and stick the rest back together, ignoring the ID at the end.
+    room = ''.join(room_data[:-4].split('-'))
+    # Get the id.
+    room_id = int(room_data[-3:])
+    # trim the trailing ']'.
+    checksum = checksum[:-1]
+    return room, room_id, checksum
