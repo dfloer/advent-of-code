508706fa02060fba47357690458b14037b8213dd
Dale Floer <dalefloer@gmail.com>
2016-12-07 01:09:09 -0800

    Added solution to day7 part2.

--- 2016/day7.py
+++ 2016/day7.py
@@ -1,5 +1,5 @@
-import re
-from collections import Counter
+import regex as re
+from collections import Counter, defaultdict
 
 
 def day7_split():
@@ -8,7 +8,7 @@ def day7_split():
     return lines
 
 
-def day7():
+def day7_part1():
     data = day7_split()
     count = 0
     for x in data:
@@ -26,8 +26,60 @@ def day7():
                     inner_count = 1
                 if idx % 2 == 1:  # Odd numbered one, inside square brackets.
                     repeated = Counter(res.group()).most_common()  # looks for repeats of the form 'aaaa'
-                    if len(repeated) != 1:  # If we have a result and it isn't a repeat, this address isn't valie.
+                    if len(repeated) != 1:  # If we have a result and it isn't a repeat, this address isn't valid.
                         inner_count = 0
                         break
         count += inner_count
     return count
+
+
+def day7_part2():
+    data = day7_split()
+    count = []
+    for x in data:
+        split = re.split(r"\[(.*?)\]", x)
+        indexes = defaultdict(list)
+        for idx in range(len(split)):
+            to_check = split[idx]
+            res = re.findall(r"(.)(.)\1", to_check, overlapped=True)  # (.)(.)(\1) returns 2 items
+            if res:
+                # Builds up a dictionary if {('a', 'b'): [1]} pairs. The value is the chunk index(es) where the words are.
+                for candidate in res:
+                    indexes[candidate] += [idx]
+        count += [check_valid(indexes)]
+    return sum(count)
+
+
+def check_valid(indexes):
+    """
+    Checks to see whether or not the string s is valid in the given indexes.
+    """
+    # Check if any substrings has any versions that are the opposite of it anywhere in that line.
+    valid = False
+    for k, v in indexes.items():
+        swapped = k[::-1]
+        other = indexes.get(swapped)
+        # Check to see if the swapped version exists in the dictionary.
+        if other:
+            # 'aaa' case, these are invalid matches, don't bother checking further.
+            if k == swapped:
+                continue
+            # single occurence case
+            if len(v) == 1 and len(other) == 1:
+                # Case where both occur inside or outsid brackets.
+                if (int(v[0]) % 2) == (int(other[0]) % 2):
+                    continue
+                else:
+                    valid = True
+            else:
+                # Use sets to eliminate duplicates in the same chunk.
+                v_s = set(v)
+                other_s = set(other)
+                # If the two sets are the same, then this is invalid.
+                if v_s == other_s:
+                    continue
+                possible_combinations = [(x % 2, y % 2) for x in v_s for y in other_s]
+                # For a pairing to be valid, one part needs to be in an even chunk and the other in an odd ([]) chunk.
+                if (1, 0) in possible_combinations or (0, 1) in possible_combinations:
+                    valid = True
+    return valid
