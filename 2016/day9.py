cf4abc91721ccdbf0fb13575797cfaed689f74f8
Dale Floer <dalefloer@gmail.com>
2016-12-08 23:09:20 -0800

    Solution to day9 part2.

--- 2016/day9.py
+++ 2016/day9.py
@@ -4,7 +4,7 @@ def day9_split():
     return lines
 
 
-def day9():
+def day9_part1():
     data = day9_split()[0]
     offset = 0
     output = ''
@@ -28,3 +28,33 @@ def day9():
             output += c
             offset += 1
     return len(output)
+
+
+def day9_part2():
+    data = day9_split()[0]
+    return day9_part2_inner(data)
+
+
+def day9_part2_inner(data):
+    offset = 0
+    output = 0
+    while True:
+        try:
+            c = data[offset]
+        except IndexError:
+            break
+        if c == '(':  # We're looking at a block to offset.
+            end = offset + 1
+            while data[end] != ')':
+                end += 1
+            end += 1
+            marker = data[offset + 1 : end - 1]
+            length, reps = marker.split('x')
+            block = data[end : end + int(length)]
+            additional = day9_part2_inner(block) * int(reps)
+            output += additional
+            offset += len(block) + len(marker) + 2
+        else:  # normal characters that aren't compressed, add as normal.
+            output += 1
+            offset += 1
+    return output
