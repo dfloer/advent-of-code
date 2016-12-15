4cb992c03cadc3db189de858da69cc5e8b7a4fb3
Dale Floer <dalefloer@gmail.com>
2016-12-14 21:37:50 -0800

    Solution to day15 part1.

---
+++ 2016/day15.py
@@ -0,0 +1,28 @@
+from collections import OrderedDict
+
+
+def day15_split():
+    with open('day15.txt', 'r') as f:
+        lines = f.read().splitlines()
+    return lines
+
+
+def day15():
+    discs = OrderedDict()
+    data = day15_split()
+    for disc in data:
+        d = disc.split()
+        i = int(d[1][-1])
+        posns = int(d[3])
+        curr = int(d[-1][:-1])
+        discs[i] = {'p': posns, 'c': curr}
+    t = 0
+    while True:
+        ps = []
+        for i,  v in enumerate(discs.values()):
+            c = v['c']
+            p = v['p']
+            ps += [(c + i + t + 1) % p]
+        if sum(ps) == 0:
+            return t
+        t += 1
