f44447a961526c00ddc9e0b1c4a1bd67b74ece8a
Dale Floer <dalefloer@gmail.com>
2016-12-19 02:25:13 -0800

    Solution to day19 part2 added.

--- 2016/day19.py
+++ 2016/day19.py
@@ -1,8 +1,28 @@
 def day19(elves):
     # Part1 appears to be the Josephus problem: https://en.wikipedia.org/wiki/Josephus_problem
-    # The algorithm is given on that page.
+    # The algorithm is given on that article, in the "Solution" section, translated from Java.
     l = elves - highest_one_bit(elves)
-    return 2 * l + 1
+    pt1 = 2 * l + 1
+
+    """
+    For part 2 I did the first 10 iterations until I spotted a pattern. The pattern looks like:
+    1 (1=3^0)
+    1,2,3 (3=3^1)
+    1,2,3,5,7,9 (9=3^2)
+    1
+    I speculated that this pattern held then worked out a mathematical solution to produce it.
+    """
+    i = 1
+    x = 0
+    while True:
+        a = (3 ** i)
+        if a > elves:
+            break
+        x = a
+        i += 1
+    pt2 = elves - x
+    # 1410968 high, 1594323 high
+    return pt1, pt2
 
 
 def highest_one_bit(n):
