afa81ad43f639fb291dc038a7dea1793702d0315
Dale Floer <dalefloer@gmail.com>
2016-12-20 23:28:57 -0800

    Solution to day21 part1.

---
+++ 2016/day21.py
@@ -0,0 +1,44 @@
+def day21_split():
+    with open('day21.txt', 'r') as f:
+        lines = f.read().splitlines()
+    return lines
+
+
+# gcedfahb
+def day21(pw='abcdefgh'):
+    data = day21_split()
+    pw = list(pw)
+    for entry in data:
+        oper = entry.split()
+        op = oper[0]
+        if op == "swap":
+            if oper[1] == "position":
+                x = int(oper[2])
+                y = int(oper[-1])
+            elif oper[1] == "letter":
+                x = pw.index(oper[2])
+                y = pw.index(oper[-1])
+            pw[x], pw[y] = pw[y], pw[x]  # Swap letter at posn x and y.
+        elif op == "rotate":
+            if oper[1] == "based":
+                x = oper[-1]
+                idx = pw.index(x)
+                rot = ((idx + 1 + (idx >= 4)) % len(pw)) * -1  # Because True == 1.
+            else:
+                dr = oper[1]
+                amt = int(oper[2])
+                rot = amt
+                if dr == "right":
+                    rot *= -1
+            pw = pw[rot:] + pw[:rot]
+        elif op == "reverse":
+            x = int(oper[2])
+            y = int(oper[-1])
+            pw[x : y + 1] = pw[x : y + 1][::-1]
+        elif op == "move":
+            x = int(oper[2])
+            y = int(oper[-1])
+            pw.insert(y, pw.pop(x))
+        else:
+            print("WTF?")
+    return ''.join(pw)
