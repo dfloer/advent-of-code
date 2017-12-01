101c6348fecf07fe358de8aea038cae07ffc6c1a
Dale Floer <dalefloer@gmail.com>
2017-12-01 02:23:22 -0800

    Solution to Day1 Part2.

--- 2017/AoC2017/Day1.cs
+++ 2017/AoC2017/Day1.cs
@@ -7,15 +7,10 @@ namespace AoC2017
         static void Main(string[] args)
         {
             string input_text = System.IO.File.ReadAllText(@"day1.txt");
-            //var split_input = input_text.ToCharArray();
             char[] split_input = input_text.ToCharArray();
-            Console.WriteLine(split_input);
-
-
-
 
             int string_length = split_input.Length;
-            int running_sum = 0;
+            int pt1_running_sum = 0;
             for (int i = 0; i < string_length - 1; i++)
             {
                 int a = (int)Char.GetNumericValue(split_input[i]);
@@ -23,18 +18,31 @@ namespace AoC2017
 
                 if (a == b)
                 {
-                    running_sum += a;
-                    Console.WriteLine("{0}: {1}, {2}", i, a, b);
+                    pt1_running_sum += a;
                 }
             }
             int first = (int)Char.GetNumericValue(split_input[0]);
             int last = (int)Char.GetNumericValue(split_input[string_length - 2]);  // Why is this -2 and not -1 for the last element?
             if (first == last)
             {
-                running_sum += first;
-                Console.WriteLine("first/last: {0}, {1}", first, last);
+                pt1_running_sum += first;
             }
-            Console.WriteLine("Day1, part1 answer: {0}", running_sum);
+
+            Console.WriteLine("Day1, part1 answer: {0}", pt1_running_sum);
+            int pt2_running_sum = 0;
+            for (int i = 0; i < string_length; i++)
+            {
+                int a = (int)Char.GetNumericValue(split_input[i]);
+                int halfway = (i + (string_length / 2)) % string_length;
+                int b = (int)Char.GetNumericValue(split_input[halfway]);
+
+                if (a == b)
+                {
+                    pt2_running_sum += a;
+                }
+            }
+
+            Console.WriteLine("Day1, part2 answer: {0}", pt2_running_sum);
 
             // Keep console window open when we end.
             Console.WriteLine("Press any key to exit.");
