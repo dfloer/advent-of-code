63f94ce5765a5e049f023afa767b8da66d124626
Dale Floer <dalefloer@gmail.com>
2017-12-04 02:47:52 -0800

    Solution to day4 part1.

---
+++ 2017/AoC2017/Day4.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace Day4
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            string line;
+            System.IO.StreamReader file = new System.IO.StreamReader(@"day4.txt");
+            int valid_passphrases = 0;
+            int i = 0;
+            while ((line = file.ReadLine()) != null)
+            {
+                string[] split_line = line.Split();
+                var results = split_line.GroupBy(x => x)
+                              .Where(g => g.Count() > 1)
+                              .Select(y => y.Key)
+                              .ToList();                
+                if (results.Count == 0)
+                {
+                    valid_passphrases += 1;
+                }
+                i++;
+            }
+            System.Console.WriteLine("Solution to Day4 Part1: {0}", valid_passphrases);
+        }
+    }
+}
