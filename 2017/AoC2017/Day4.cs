d87d3c7ca8a445a5598567f9d67ffa9041a8d641
Dale Floer <dalefloer@gmail.com>
2017-12-04 03:14:08 -0800

    Solution to day4 part2.

--- 2017/AoC2017/Day4.cs
+++ 2017/AoC2017/Day4.cs
@@ -9,8 +9,8 @@ namespace Day4
         {
             string line;
             System.IO.StreamReader file = new System.IO.StreamReader(@"day4.txt");
-            int valid_passphrases = 0;
-            int i = 0;
+            int pt1_valid_passphrases = 0;
+            int pt2_valid_passphrases = 0;
             while ((line = file.ReadLine()) != null)
             {
                 string[] split_line = line.Split();
@@ -20,11 +20,35 @@ namespace Day4
                               .ToList();                
                 if (results.Count == 0)
                 {
-                    valid_passphrases += 1;
+                    pt1_valid_passphrases += 1;
+                    if (IsAnagram(split_line) == false)
+                    {
+                        pt2_valid_passphrases += 1;
+                    }
                 }
-                i++;
             }
-            System.Console.WriteLine("Solution to Day4 Part1: {0}", valid_passphrases);
+            System.Console.WriteLine("Solution to Day4 Part1: {0}", pt1_valid_passphrases);
+            System.Console.WriteLine("Solution to Day4 Part1: {0}", pt2_valid_passphrases);
+        }
+
+        static bool IsAnagram(string[] line_string)
+        {
+            for(int i = 0; i < line_string.Length; i++)
+            {
+                for(int j = i + 1; j < line_string.Length; j++)
+                {
+                    char[] a = line_string[i].ToCharArray();
+                    char[] b = line_string[j].ToCharArray();
+                    Array.Sort(a);
+                    Array.Sort(b);
+                    // Not sure exactly why a == b doesn't work here, but this does work correctly.
+                    if (a.SequenceEqual(b))
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
         }
     }
 }
