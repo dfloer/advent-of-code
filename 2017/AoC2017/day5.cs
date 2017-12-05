d6b2a0d42c9aedab8d310c8b1b14fe25efa982a6
Dale Floer <dalefloer@gmail.com>
2017-12-05 01:07:34 -0800

    Solution to day5 part2.

---
+++ 2017/AoC2017/day5.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Day5
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            string line;
+            List<int> inst = new List<int>();
+            System.IO.StreamReader file = new System.IO.StreamReader(@"day5.txt");
+            while ((line = file.ReadLine()) != null)
+            {
+                int value = int.Parse(line);
+                inst.Add(value);
+            }
+            
+            int inst_idx = 0;
+            int step_count = 0;
+            while(true)
+            {
+                try
+                {
+                    int offset = inst[inst_idx];
+                    inst[inst_idx] = offset + 1;
+                    inst_idx += offset;
+                    step_count += 1;
+                }
+                catch (System.ArgumentOutOfRangeException)
+                {
+                    System.Console.WriteLine("Solution to Day5 Part1: {0}", step_count);
+                    break;
+                }
+            }
+        }
+    }
+}
