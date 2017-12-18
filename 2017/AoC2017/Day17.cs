9436e4abfbcc4c016580be25aeffdff77c6ab5f9
Dale Floer <dalefloer@gmail.com>
2017-12-17 17:33:32 -0800

    solution to day17 part2.

--- 2017/AoC2017/Day17.cs
+++ 2017/AoC2017/Day17.cs
@@ -20,7 +20,20 @@ namespace Day17
                 spinlock.Insert(pos_idx, i);                
             }
             int pt1 = spinlock[spinlock.IndexOf(2017) + 1];
-            System.Console.Write("Solution to Day17 Part1: {0}", pt1);
+            System.Console.WriteLine("Solution to Day17 Part1: {0}", pt1);
+
+            circular_len = 50000000;
+            spinlock = new List<int>();
+            pos_idx = 0;
+            int candidate = 0;
+            foreach (int i in Enumerable.Range(1, circular_len))
+            {
+                pos_idx = (pos_idx + input) % i;
+                if (pos_idx == 0)
+                    candidate = i;
+                pos_idx++;
+            }
+            System.Console.WriteLine("Solution to Day17 Part2: {0}", candidate);
         }
     }
 }
