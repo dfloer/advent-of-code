bdf6bd7e214d0cae1a9d7f3922a1ff58948d01c1
Dale Floer <dalefloer@gmail.com>
2017-12-21 21:10:36 -0800

    WiP string version of day21.

--- 2017/AoC2017/Day21.cs
+++ 2017/AoC2017/Day21.cs
@@ -11,27 +11,27 @@ namespace Day21
             System.IO.StreamReader file = new System.IO.StreamReader(@"day21.txt");
             string raw_line;
             int iters = 5;
-            Dictionary<List<List<char>>, List<List<char>>> rules = new Dictionary<List<List<char>>, List<List<char>>>();
-            List<List<char>> grid = new List<List<char>>();
-            grid.Add(".#.".ToCharArray().ToList());
-            grid.Add("..#".ToCharArray().ToList());
-            grid.Add("###".ToCharArray().ToList());
+            Dictionary<List<string>, List<string>> rules = new Dictionary<List<string>, List<string>>();
+            List<string> grid = new List<string>();
+            grid.Add(".#.".ToString());
+            grid.Add("..#".ToString());
+            grid.Add("###".ToString());
             int xxx = 0;
             while ((raw_line = file.ReadLine()) != null)
             {
                 List<string> temp = raw_line.Replace(" ", String.Empty).Split(new string[] { "=>" }, StringSplitOptions.None).ToList();
                 string first = temp.First();
                 string last = temp.Last();
-                List<List<char>> match_rule = parse_rule(first);
-                List<List<char>> rule_result = parse_rule(last);
+                List<string> match_rule = parse_rule(first);
+                List<string> rule_result = parse_rule(last);
                 rules[match_rule] = rule_result;
                 //Handle the 3 rotations and 2 flips right now.
-                List<List<char>> rotate_90 = rotate(match_rule); // Doesn't rotate. ???
-                List<List<char>> rotate_180 = rotate(rotate_90); // Actually 90.
-                List<List<char>> rotate_270 = rotate(rotate_180); // actually 180.
-                //List<List<char>> rotate_360 = rotate(rotate_270); // Actually 270.
-                List<List<char>> flip_vertical = flip_vert(match_rule);
-                List<List<char>> flip_horizontal = flip_horiz(match_rule);
+                List<string> rotate_90 = rotate(match_rule); // Doesn't rotate. ???
+                List<string> rotate_180 = rotate(rotate_90); // Actually 90.
+                List<string> rotate_270 = rotate(rotate_180); // actually 180.
+                //List<string> rotate_360 = rotate(rotate_270); // Actually 270.
+                List<string> flip_vertical = flip_vert(match_rule);
+                List<string> flip_horizontal = flip_horiz(match_rule);
                 rules[rotate_90] = rule_result;
                 rules[rotate_180] = rule_result;
                 rules[rotate_270] = rule_result;
@@ -68,7 +68,7 @@ namespace Day21
             System.Console.WriteLine("Rules size: {0}", rules.Count);
             for (int i = 0; i < iters; i++)
             {
-                int n = grid[0].Count;
+                int n = grid[0].Length;
                 int step = 2;
                 int new_dim = n * (3 / 2);
                 if (n % 3 == 0)
@@ -77,19 +77,19 @@ namespace Day21
                     new_dim = n * (4 / 3);
                 }
                 //Create and initialize blank results 2D list.
-                List<List<char>> results = new List<List<char>>();
+                List<string> results = new List<string>();
                 for(int j = 0; j < new_dim; j++)
                 {
-                    List<char> line = new List<char>();
+                    string line = "";
                     for (int k = 0; k < new_dim; k++)
-                        line.Add(' ');
+                        line += " ";
                     results.Add(line);
                 }
                 for (int row_idx = 0; row_idx < n; row_idx += step)
                 {
                     for (int col_idx = 0; col_idx < n; col_idx += step)
                     {
-                        List<List<char>> square = grab_square(grid, row_idx, col_idx, step);
+                        List<string> square = grab_square(grid, row_idx, col_idx, step);
                         print_array(square);
                         square = rules[square];
                         results = add_square(square, results, row_idx, col_idx, step);
@@ -98,77 +98,77 @@ namespace Day21
                 grid = results;
             }
             int pt1 = 0;
-            foreach (List<char> line in grid)
+            foreach (string line in grid)
                 foreach (char x in line)
                     if (x == '#')
                         pt1++;
             System.Console.WriteLine("Solution to Day21 Part1: {0}", pt1);
         }
-        static List<List<char>> parse_rule(string input_string)
+        static List<string> parse_rule(string input_string)
         {
             var rule = input_string.Split('/').ToList();
-            List<List<char>> res = new List<List<char>>();
+            List<string> res = new List<string>();
             foreach (var line in rule)
-                res.Add(line.ToCharArray().ToList());
+                res.Add(line.ToString());
             return res;
         }
-        static List<List<char>> rotate(List<List<char>> arr)
+        static List<string> rotate(List<string> arr)
         {
 
-            int n = arr[0].Count;
-            List<List<char>> res = new List<List<char>>();
+            int n = arr[0].Length;
+            List<string> res = new List<string>();
             /*for (int i = 0; i < n; ++i)
             {
-                List<char> line = new List<char>();
+                string line = new string();
                 for (int j = 0; j < n; ++j)
                     line.Add(' ');
                 res.Add(line);
             }*/
             for (int i = 0; i < n; ++i)
             {
-                List<char> line = new List<char>();
+                string line = "";
                 for (int j = 0; j < n; ++j)
-                    line.Add(arr[n - j - 1][i]);
+                    line += arr[n - j - 1][i];
                 res.Add(line);
             }
             return res;
         }
-        static List<List<char>> flip_vert(List<List<char>> arr)
+        static List<string> flip_vert(List<string> arr)
         {
-            List<List<char>> res = new List<List<char>>();
-            int n = arr[0].Count;
+            List<string> res = new List<string>();
+            int n = arr[0].Length;
             for (int i = 0; i < n; i++)
             {
                 res.Add(arr[n - 1 - i]);
             }
             return res;
         }
-        static List<List<char>> flip_horiz(List<List<char>> arr)
+        static List<string> flip_horiz(List<string> arr)
         {
-            List<List<char>> res = new List<List<char>>();
-            int n = arr[0].Count;
+            List<string> res = new List<string>();
+            int n = arr[0].Length;
             for (int i = 0; i < n; i++)
             {
-                List<char> tt = arr[i].ToList();
+                string tt = arr[i];
                 tt.Reverse();
                 res.Add(tt);
             }
             return res;
         }
-        static List<List<char>> grab_square(List<List<char>> arr, int row_idx, int col_idx, int n)
+        static List<string> grab_square(List<string> arr, int row_idx, int col_idx, int n)
         {
-            List<List<char>> res = new List<List<char>>();
+            List<string> res = new List<string>();
             for(int i = row_idx; i < row_idx + n; i++)
             {
-                List<char> row = arr[i];
-                List<char> new_row = row.Skip(col_idx).Take(n).ToList();
+                string row = arr[i];
+                string new_row = row.Skip(col_idx).Take(n).ToString();
                 res.Add(new_row);
             }
             return res;
         }
-        static List<List<char>> add_square(List<List<char>> arr, List<List<char>> result, int old_row_idx, int old_col_idx, int steps)
+        static List<string> add_square(List<string> arr, List<string> result, int old_row_idx, int old_col_idx, int steps)
         {
-            int dim = arr[0].Count;
+            int dim = arr[0].Length;
             int new_row_idx = old_row_idx * (3 / 2);
             int new_col_idx = old_col_idx * (3 / 2);
             if (steps == 3)
@@ -176,14 +176,19 @@ namespace Day21
                 new_row_idx = old_row_idx * (4 / 3);
                 new_col_idx = old_col_idx * (4 / 3);
             }
-            for(int i = 0; i < dim; i++)
-                for(int j = 0; j < dim; j++)
-                    result[new_row_idx + i][new_col_idx + j] = arr[i][j]; 
+            for (int i = 0; i < dim; i++)
+                for (int j = 0; j < dim; j++)
+                {
+                    int x = new_row_idx + i;
+                    char[] temp = result[x].ToCharArray();
+                    temp[new_col_idx + j] = arr[i][j];
+                    result[x] = temp.ToString();
+                }
             return result;
         }
-        static void print_array(List<List<char>> arr)
+        static void print_array(List<string> arr)
         {
-            foreach (List<char> line in arr)
+            foreach (string line in arr)
                 System.Console.WriteLine("[{0}]", String.Join("", line));
         }
     }
