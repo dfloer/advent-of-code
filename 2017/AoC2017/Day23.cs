9bd7068d5798f007aa4fe6025b2fcd0dd93ebed1
Dale Floer <dalefloer@gmail.com>
2017-12-23 00:05:13 -0800

    Solution to Day23 Part2.
    Solution to part1 updated as well.

--- 2017/AoC2017/Day23.cs
+++ 2017/AoC2017/Day23.cs
@@ -11,7 +11,6 @@ namespace Day23
             System.IO.StreamReader file = new System.IO.StreamReader(@"day23.txt");
             string raw_line;
             Dictionary<char, long> registers = new Dictionary<char, long>();
-            Stack<long> freqs = new Stack<long>();
             List<string> instructions = new List<string>();
             foreach (char c in "abcdefgh")
                 registers[c] = 0;
@@ -19,8 +18,23 @@ namespace Day23
             {
                 instructions.Add(raw_line);
             }
+            long pt1 = part1(registers, instructions);
+            System.Console.WriteLine("Solution to Day23 Part2: {0}", pt1);
+            registers['a'] = 1;
+            foreach (char c in "bcdefgh")
+                registers[c] = 0;
+            registers = part2(registers, instructions);
+            System.Console.WriteLine("Solution to Day23 Part2: {0}", registers['h']);
+        }
+        static long part1(Dictionary<char, long> registers, List<string> instructions)
+        {
+            List<string> split_line = instructions[0].Split(' ').ToList();
+            long initial_value = long.Parse(split_line[2]);
+            return (long)Math.Pow(initial_value - 2, 2);
+        }
+        static Dictionary<char, long> part2(Dictionary<char, long> registers, List<string> instructions)
+        {
             int program_counter = 0;
-            long pt1_mul_count = 0;
             while (program_counter < instructions.Count())
             {
                 List<string> split_line = instructions[program_counter].Split(' ').ToList();
@@ -47,19 +61,44 @@ namespace Day23
                         break;
                     case "mul":
                         registers[reg] *= value;
-                        pt1_mul_count++;
                         break;
                     case "jnz":
                         long X = 1;
                         if (reg != '1')
                             X = registers[reg];
                         if (X != 0)
-                        pc_offset = (int)value;
+                            pc_offset = (int)value;
                         break;
                 }
                 program_counter += pc_offset;
+                //Optimization time. When we've hit instruction 8, the program spends a lot of time doing stuff.
+                if (program_counter == 8)
+                    break;
             }
-            System.Console.WriteLine("Solution to Day23 Part2: {0}", pt1_mul_count);
+            long low_val = registers['b'];
+            long high_val = registers['c'];
+            /* When I was attempting to reverse engineer the program, I saw that it generated two numbers in reg b and c.
+             * And then when a number between them was prime, it didn't increment register h.
+             * Not sure where the increment of 17 comes from, but it's what my input used.
+             * So register h should be the number of non-primes between the numbers in reg b and c.*/
+            for (long i = low_val; i <= high_val; i += 17)
+                if (!is_prime(i))
+                    registers['h']++;
+            return registers;
+        }
+        static bool is_prime(long n)
+        {
+            if (n == 1)
+                return false;
+            if (n == 2)
+                return true;
+            if (n % 2 == 0)
+                return false;
+            var half = (int)Math.Floor(Math.Sqrt(n));
+            for (int i = 3; i <= half; i += 2)
+                if (n % i == 0)
+                    return false;
+            return true;
         }
     }
 }
