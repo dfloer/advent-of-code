614f5f3994197df60c06d9a0d3182659f0ac0c30
Dale Floer <dalefloer@gmail.com>
2018-12-01 00:04:03 -0800

    Solution to day1 part2.

--- 2018/day1.py
+++ 2018/day1.py
@@ -4,13 +4,29 @@ def day1_split():
     return lines
 
 
-def day1(start=0):
+def day1_part1(start=0):
     freq = start
     for l in day1_split():
         x = int(l)
-        freq +=x
+        freq += x
     return freq
 
 
+def day1_part2(start=0):
+    freq = start
+    freqs = set()
+    updates = day1_split()
+    idx = 0
+    while True:
+        x = int(updates[idx % len(updates)])
+        freq += x
+        if freq in freqs:
+            return freq
+        else:
+            freqs.add(freq)
+        idx += 1
+
+
 if __name__ == "__main__":
-    print(f"Solution to day 1 part 1: {day1()}")
+    print(f"Solution to day 1 part 1: {day1_part1()}")
+    print(f"Solution to day 1 part 1: {day1_part2()}")
