26aad92538b4f4735465faf0ac6127a8e8dd662d
Dale Floer <dalefloer@gmail.com>
2018-12-01 22:01:14 -0800

    Solution to day2 part2.

--- 2018/day2.py
+++ 2018/day2.py
@@ -1,4 +1,5 @@
 from collections import Counter
+from difflib import ndiff
 
 
 def day2_split():
@@ -13,6 +14,11 @@ def day2_part1():
     return checksum(lines)
 
 
+def day2_part2():
+    lines = day2_split()
+    return str_cmp(lines)
+
+
 def checksum(lines):
     twos = 0
     threes = 0
@@ -25,5 +31,21 @@ def checksum(lines):
     return twos * threes
 
 
+def str_cmp(lines):
+    length = len(lines[0])
+    for x in range(len(lines)):
+        for y in range(len(lines) - 1):
+            diff = list(ndiff(lines[x], lines[y]))
+            if len(diff) == length + 1:
+                res = ''
+                for x in diff:
+                    if x[0] in ('-', '+'):
+                        continue
+                    else:
+                        res += x[2]
+                return res
+
+
 if __name__ == "__main__":
     print(f"Solution to day 2 part 1: {day2_part1()}")
+    print(f"Solution to day 2 part 1: '{day2_part2()}'")
