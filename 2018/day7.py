fed074711e9084110a4f55ef03e177794b200512
Dale Floer <dalefloer@gmail.com>
2018-12-07 04:12:38 -0800

    Solution to day7 part 2.

--- 2018/day7.py
+++ 2018/day7.py
@@ -12,7 +12,7 @@ def parse_input():
     return res
 
 
-def day7():
+def day7_part1():
     lines = parse_input()
     all_steps = set([item for sublist in lines for item in sublist])
 
@@ -20,7 +20,7 @@ def day7():
     while all_steps:
         candidates = list(set([x[0] for x in lines]) - set([x[1] for x in lines]))
         # Special handling for the end/last item.
-        if candidates is []:
+        if candidates == []:
             candidates = list(all_steps)
         candidates.sort()
         next_step = candidates[0]
@@ -31,6 +31,31 @@ def day7():
     return step_ordering
 
 
+def day7_part2(step_duration=60, num_workers=5):
+    lines = parse_input()
+    all_steps = set([item for sublist in lines for item in sublist])
+    elves = [0 for _ in range(num_workers)]
+    current_work = [None for _ in elves]
+
+    time_spent = 0
+    while all_steps or sum(elves) > 0:
+        done = [x[1] for x in lines]
+        candidates = [s for s in all_steps if s not in done]
+        for x in range(num_workers):
+            elves[x] = max(elves[x] - 1, 0)
+            if elves[x] == 0:
+                e = current_work[x]
+                if e is not None:
+                    lines = [(a, b) for a, b in lines if a != e]
+                if candidates:
+                    next_step = candidates.pop(0)
+                    elves[x] = step_duration + ord(next_step) - ord('A')
+                    current_work[x] = next_step
+                    all_steps.remove(next_step)
+        time_spent += 1
+    return time_spent
+
+
 if __name__ == "__main__":
-    print(f"Solution to day 7 part 1: {day7()}")
-    # print(f"Solution to day 7 part 2: {pt2}")
+    print(f"Solution to day 7 part 1: {day7_part1()}")
+    print(f"Solution to day 7 part 2: {day7_part2()}")
