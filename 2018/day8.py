92269defcbb783b407cb99b69786b6e737ccc58f
Dale Floer <dalefloer@gmail.com>
2018-12-08 01:50:34 -0800

    Solution to day 8 part 2.

--- 2018/day8.py
+++ 2018/day8.py
@@ -6,13 +6,27 @@ def day8_split():
 
 def parse_node(node_data):
     metadata = []
+    metadata_values = []
     child_count, metadata_count = node_data[ : 2]
     node_data = node_data[2 : ]
     for _ in range(child_count):
-        x, node_data = parse_node(node_data)
+        x, node_data, values = parse_node(node_data)
         metadata += x
-    metadata += node_data[ : metadata_count]
-    return metadata, node_data[metadata_count : ]
+        metadata_values += [values]
+    new_metadata = node_data[ : metadata_count]
+    metadata += new_metadata
+
+    # Find the value of the node.
+    if child_count == 0:
+        node_value = new_metadata
+    else:
+        n = []
+        for x in new_metadata:
+            # Check and see if the node references makes sense, otherwise don't bother.
+            if 0 < x <= len(metadata_values):
+                n += metadata_values[x - 1]
+        node_value = n
+    return metadata, node_data[metadata_count : ], node_value
 
 
 def day8_parse():
@@ -22,10 +36,11 @@ def day8_parse():
 
 
 def day8():
-    data = day8_parse()
-    return sum([item for sublist in data for item in sublist])
+    part1, _, part2 = day8_parse()
+    return sum(part1), sum(part2)
 
 
 if __name__ == "__main__":
-    print(f"Solution to day 8 part 1: {day8()}")
-    #print(f"Solution to day 8 part 2: {day8_part2()}")
+    pt1, pt2 = day8()
+    print(f"Solution to day 8 part 1: {pt1}")
+    print(f"Solution to day 8 part 2: {pt2}")
