0595b262c924dd30090c474469095d2d67afa402
Dale Floer <dalefloer@gmail.com>
2018-12-09 11:02:52 -0800

    Solution to day 9 part 2.

--- 2018/day9.py
+++ 2018/day9.py
@@ -7,11 +7,11 @@ def day9_split():
     return int(data[0]), int(data[6])
 
 
-def day9():
+def day9(extra=1):
     total_players, last_points = day9_split()
     players = {x : 0 for x in range(total_players)}
     marbles = deque([0])
-    for v in range(1, last_points + 1):
+    for v in range(1, (last_points + 1) * extra):
         player = v % total_players
         if v % 23 == 0:
             players[player] += v
@@ -26,4 +26,4 @@ def day9():
 
 if __name__ == "__main__":
     print(f"Solution to day 9 part 1: {day9()}")
-    #print(f"Solution to day 9 part 2: {pt2}")
+    print(f"Solution to day 9 part 2: {day9(100)}")
