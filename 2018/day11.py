446b86d067fe22ad4abe0730a5300b77e46d8388
Dale Floer <dalefloer@gmail.com>
2018-12-10 23:01:12 -0800

    Solution to day 11 part 2. It's slooooooow.

--- 2018/day11.py
+++ 2018/day11.py
@@ -14,23 +14,40 @@ def day11():
     for x in range(1, 301):
         for y in range(1, 301):
             fuel_cells[(x, y)] = power_level(x, y, serial)
+    max_idx, _ = find_max(fuel_cells, 3)
+    max_max_idx = (0, 0)
+    max_size = 0
+    max_max_val = 0
+    # This solution is very slow, I didn't wait for it to finish but found the first answer that looked right and submitted it. It was correct.
+    for square_size in range(1, 301):
+        idx, val = find_max(fuel_cells, square_size)
+        print(idx, square_size)
+        if val > max_max_val:
+            max_max_val = val
+            max_size = square_size
+            max_max_idx = idx
+    a, b = max_idx
+    c, d = max_max_idx
+    e = max_size
+    return f"{a},{b}", f"{c},{d},{e}"
 
+
+def find_max(fuel_cells, square_size):
     max_idx = (0, 0)
     max_val = 0
-    for x in range(1, 298):
-        for y in range(1, 298):
-            t = square_total(x, y, fuel_cells)
+    for x in range(1, 301 - square_size):
+        for y in range(1, 301 - square_size):
+            t = square_total(x, y, fuel_cells, square_size)
             if t > max_val:
                 max_val = t
                 max_idx = (x, y)
-    a, b = max_idx
-    return f"{a},{b}"
+    return (max_idx, max_val)
 
 
-def square_total(x, y, d):
+def square_total(x, y, d, s):
     n = 0
-    for a in range(x, x + 3):
-        for b in range(y, y + 3):
+    for a in range(x, x + s):
+        for b in range(y, y + s):
             n += d[(a, b)]
     return n
 
@@ -49,5 +66,6 @@ def power_level(x, y, serial):
 
 
 if __name__ == "__main__":
-    print(f"Solution to day 11 part 1: {day11()}")
-    #print(f"Solution to day 11 part 2: {day11()}")
+    pt1, pt2 = day11()
+    print(f"Solution to day 8 part 1: {pt1}")
+    print(f"Solution to day 8 part 2: {pt2}")
