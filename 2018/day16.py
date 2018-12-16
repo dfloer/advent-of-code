8e28d3fc807e6fc58504901092ec9a20758c513a
Dale Floer <dalefloer@gmail.com>
2018-12-16 00:08:57 -0800

    Solution to day 16 part 2 (super hacky).

--- 2018/day16.py
+++ 2018/day16.py
@@ -11,20 +11,26 @@ def parse():
     raw = day16_split()
     offset = 0
     instructions = {}
-    while offset < 3244:
+    while True:
         before = [int(x) for x in re.findall(r'-?\d+', raw[offset])]
         op = [int(x) for x in re.findall(r'-?\d+', raw[offset + 1])]
         after = [int(x) for x in re.findall(r'-?\d+', raw[offset + 2])]
-        instructions[offset] = {"before": before, "op": op, "after": after}
         offset += 4
-    return instructions
+        if [] in (before, op, after):
+            offset -= 1
+            break
+        instructions[offset] = {"before": before, "op": op, "after": after}
+
+    program = {}
+    for idx, line in enumerate(raw[offset - 1 : ]):
+        program[idx + offset] = [int(x) for x in re.findall(r'-?\d+', line)]
+    return instructions, program
 
 
 opcodes = ["addr", "addi", "mulr", "muli", "banr", "bani", "borr", "bori", "setr", "seti", "gtir", "gtri", "gtrr", "eqir", "eqri", "eqrr"]
 
 
-def day16_part1():
-    instructions = parse()
+def match_opcodes(instructions):
     matches = defaultdict(list)
     for k, v in instructions.items():
         for opcode in opcodes:
@@ -33,11 +39,50 @@ def day16_part1():
             expected = v["after"]
             if result == expected:
                 matches[k] += [opcode]
+    return matches
+
+
+def day16_part1():
+    instructions, program = parse()
+    matches = match_opcodes(instructions)
     total = 0
     for m in matches.values():
         if len(m) >= 3:
             total += 1
     return total
+
+
+def day16_part2():
+    instructions, program = parse()
+    matches = match_opcodes(instructions)
+    opcode_mapping = {}
+    for idx in range(16):
+        single = [(n, m) for n, m in matches.items() if len(m) == 1]
+        opcode_number = instructions[single[0][0]]["op"][0]
+        opcode_name = single[0][1][0]
+        opcode_mapping[opcode_number] = opcode_name
+        matches = {k: [x for x in v if x != opcode_name] for k, v in matches.items()}
+    if len(opcodes) > len(opcode_mapping):
+        missing_opcode = set(opcodes) - set(opcode_mapping.values())
+        missing_opcode_number = set(range(len(opcodes))) - set(opcode_mapping.keys())
+        opcode_mapping[int(list(missing_opcode_number)[0])] = str(list(missing_opcode)[0])
+
+    # This hack almost certainly only works on my input, but somehow these two are getting swapped, and I couldn't see the bug.
+    temp = opcode_mapping[0]
+    opcode_mapping[0] = opcode_mapping[12]
+    opcode_mapping[12] = temp
+    registers = run_program(program, opcode_mapping)
+
+    return registers[0]
+
+
+def run_program(program, opcode_decoder):
+    registers = [0, 0, 0, 0]
+    for inst in program.values():
+        op, a, b, c = inst
+        op_num = opcode_decoder[op]
+        registers = run_opcode(op_num, a, b, c, registers)
+    return registers
 
 
 def run_opcode(opcode, a, b, c, registers):
@@ -94,3 +139,4 @@ def run_opcode(opcode, a, b, c, registers):
 
 if __name__ == "__main__":
     print(f"Solution to day 16 part 1: {day16_part1()}")
+    print(f"Solution to day 16 part 2: {day16_part2()}")
