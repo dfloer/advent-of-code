b214feba188784922dbf0c25d8e85973a85bdfb9
Dale Floer <dfloer@gmail.com>
2018-12-21 00:15:50 -0700

    Solution to day 21 part 2.

--- 2018/day21.py
+++ 2018/day21.py
@@ -18,12 +18,15 @@ def parse():
     return ip, res
 
 
-def day21():
+def day21(part_2=False):
     """
     After the previous version didn't work, I spent some time reverse engineering the code.
     Hopefully the code is descriptive enough as to what the algorithm is actually doing.
     I was helped along by the only instruction accessing r0 being the eqrr instruction.
+
+    The first segment is my attempt to make this a general problem, but other people's input may differ in terms of which line the constants are all on.
     """
+    max_runs = 50000
     _, instructions = parse()
     seti_input = instructions[7][1]
     bani_1 = instructions[10][2]
@@ -31,20 +34,33 @@ def day21():
     or_test = instructions[6][2]
     muli_test = instructions[11][2]
     d = 0
+    pt2_candidates = set()
+    others = []
+    i = 0
     while True:
         e = d | or_test
         d = seti_input
         while True:
+            i += 1
             c = e & 255
             d += c
             d &= bani_1
             d *= muli_test
             d &= bani_2
-            if 256 > e:
+            if 256 > e and not part_2:
                 return d
+            elif 256 > e:
+                if d not in pt2_candidates:
+                    others += [d]
+                pt2_candidates.add(d)
+                break
             c = 0
+            e = e // 256  # Needed for performance.
+            # This is a hack to make the program end, no idea if it works for anything other than my input.
+            if i >= max_runs:
+                return list(others)[-1]
 
 
 if __name__ == "__main__":
     print(f"Solution to day 21 part 1: {day21()}")
-    # print(f"Solution to day 21 part 2: {}")
+    print(f"Solution to day 21 part 2: {day21(True)}")
